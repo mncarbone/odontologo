4725e737580aafb1bbc165784cd341acc9ec8ad9
unknown <Netbook@BNGH-1198.ijmestrada.edu.ar>
2017-09-12 21:28:28 -0300

    Agregado mnetodo para guardar al hacer click en save

--- odontologo.py
+++ odontologo.py
@@ -5,8 +5,12 @@ class SistemaOdontologo(QApplication):
 
     def iniciar(self):
         self.ui = loadUi('pacientes.ui')
+        self.ui.buttonBox.accepted.connect(self.btnBox_accepted)
         self.ui.show()
         self.exec()
+
+    def btnBox_accepted(self):
+        print('Guardando registro en la base de datos...')
 
 app = SistemaOdontologo([])
 app.iniciar()
