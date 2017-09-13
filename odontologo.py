d1474fb4f65e6d1f5a7b82bdd2b494b71287aae1
unknown <Netbook@BNGH-1198.ijmestrada.edu.ar>
2017-09-12 22:01:23 -0300

    Agregada la posibilidad de guardar en la DB

--- odontologo.py
+++ odontologo.py
@@ -1,5 +1,6 @@
 from PyQt4.QtGui import QApplication
 from PyQt4.uic import loadUi
+from sqlite3 import connect
 
 class SistemaOdontologo(QApplication):
 
@@ -11,6 +12,19 @@ class SistemaOdontologo(QApplication):
 
     def btnBox_accepted(self):
         print('Guardando registro en la base de datos...')
-
+        #Obtener datos de la UI
+        dni = self.ui.txtDni.text()
+        nombre = self.ui.txtNombre.text()
+        apellido = self.ui.txtApellido.text()
+        #Armar SQL
+        sql = "INSERT INTO pacientes(dni, nombre, apellido) "
+        sql+= "VALUES ('" + dni + "', '" + nombre + "', '" + apellido + "');"
+        #Ejecutar SQL
+        conexion = connect('odontologo.db')
+        cursor = conexion.cursor()
+        cursor.execute(sql)
+        conexion.commit()
+        conexion.close()
+        
 app = SistemaOdontologo([])
 app.iniciar()
