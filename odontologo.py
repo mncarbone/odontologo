c2b8fb298e8cae2645e6123da02b58da287f176d
unknown <Netbook@BNGH-1198.ijmestrada.edu.ar>
2017-09-12 21:21:26 -0300

    Mostrar la Interfaz gráfica

--- odontologo.py
+++ odontologo.py
@@ -1,3 +1,12 @@
 from PyQt4.QtGui import QApplication
 from PyQt4.uic import loadUi
 
+class SistemaOdontologo(QApplication):
+
+    def iniciar(self):
+        self.ui = loadUi('pacientes.ui')
+        self.ui.show()
+        self.exec()
+
+app = SistemaOdontologo([])
+app.iniciar()
